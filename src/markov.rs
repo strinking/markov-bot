489b948b1e268a914c22f5b12cbc95785b921b03
Ahmed Mohamed <ahmed02mohamed@outlook.com>
2017-06-27 16:35:15 -0500

    Fixed variable naming

--- src/markov.rs
+++ src/markov.rs
@@ -4,7 +4,12 @@ use std::hash::Hash;
 use typemap::Key;
 use regex::Regex;
 
-fn get_next_word<'a, T: Eq + Hash>(map: &'a HashMap<T, u32>, rng: &mut Rng) -> Option<&'a T> {
+lazy_static! {
+    static ref URL_REGEX: Regex = Regex::new(r"[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)").unwrap();
+}
+
+
+fn roulette_wheel<'a, T: Eq + Hash>(map: &'a HashMap<T, u32>, rng: &mut Rng) -> Option<&'a T> {
     let sum = map.values().sum::<u32>() as f32;
     let mut rand = rng.next_f32();
     for (key, val) in map.iter() {
@@ -20,8 +25,8 @@ fn get_next_word<'a, T: Eq + Hash>(map: &'a HashMap<T, u32>, rng: &mut Rng) -> O
 }
 
 pub struct Markov {
-    word_map: HashMap<String, HashMap<String, u32>>,
-    starting_words: HashMap<String, u32>,
+    assoc_map: HashMap<String, HashMap<String, u32>>,
+    starting_words: HashMap<String, u32>
 }
 
 impl Key for Markov {
@@ -31,22 +36,20 @@ impl Key for Markov {
 impl Markov {
     pub fn new() -> Markov {
         Markov {
-            word_map: HashMap::new(),
+            assoc_map: HashMap::new(),
             starting_words: HashMap::new(),
         }
     }
 
     pub fn parse(&mut self, string: &str) {
-        let url_regex = Regex::new(r"[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)").unwrap();
-        let string = url_regex.replace_all(string, "");
-        
+        let string = URL_REGEX.replace_all(string, "");
         let mut words = string.split(' ');
-        let mut previous_word: String;
-        
+        let mut prev: String;
+
         match words.next() {
             Some(word) => {
-                previous_word = String::from(word);
-                let count = self.starting_words.entry(previous_word.clone()).or_insert(0);
+                prev = String::from(word);
+                let count = self.starting_words.entry(prev.clone()).or_insert(0);
                 *count += 1;
             }
 
@@ -60,26 +63,26 @@ impl Markov {
         match words.next() {
             Some(word) => {
                 next_word = String::from(word);
-                self.associate(previous_word.clone(), next_word.clone());
+                self.associate(prev.clone(), next_word.clone());
             }
 
             None => {
-                self.associate(previous_word.clone(), String::from(""));
+                self.associate(prev.clone(), String::from(""));
                 return;
             }
         }
 
         for word in words {
-            previous_word = next_word.clone();
+            prev = next_word.clone();
             next_word = String::from(word);
-            self.associate(previous_word, next_word.clone());
+            self.associate(prev, next_word.clone());
         }
         self.associate(next_word.clone(), String::from(""));
     }
 
     #[inline]
-    fn associate(&mut self, previous_word: String, next_word: String) {
-        let probability = self.word_map.entry(previous_word).or_insert_with(HashMap::new);
+    fn associate(&mut self, prev: String, next_word: String) {
+        let probability = self.assoc_map.entry(prev).or_insert_with(HashMap::new);
         let count = probability.entry(next_word).or_insert(0);
         *count += 1;
     }
@@ -95,7 +98,7 @@ impl Markov {
             }
 
             None => {
-                match get_next_word(&self.starting_words, &mut rng) {
+                match roulette_wheel(&self.starting_words, &mut rng) {
                     Some(start) => {
                         word = start;
                     }
@@ -110,20 +113,16 @@ impl Markov {
         let mut result = String::new();
 
         for _ in 0..length {
-            match self.word_map.get(word) {
-                Some(probability) => {
+            match self.assoc_map.get(word) {
+                Some(probs) => {
                     if !result.is_empty() {
                         result.push(' ');
                     }
                     result.push_str(&*word);
 
-                    word =
-                        get_next_word(&probability, &mut rng).expect("Probability map is empty");
+                    word = roulette_wheel(&probs, &mut rng).expect("Probability map is empty");
 
                     if word.is_empty() {
-                        if rng.gen::<bool>() {
-                            result.push('.');
-                        }
                         break;
                     }
                 }
