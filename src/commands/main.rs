2d4db5efc687d5c9585bc296e9827fbcc67ac421
Ahmed Mohamed <ahmed02mohamed@outlook.com>
2017-06-29 21:57:56 -0500

    Fixed help command

--- src/commands/main.rs
+++ src/commands/main.rs
@@ -1,19 +1,20 @@
 use serenity::utils::MessageBuilder;
 
-command!(help(ctx, msg, args) {
+command!(help(ctx, message, args) {
     let _ = ctx;
     let _ = args;
 
-    let message = MessageBuilder::new()
-        .mention(msg.author.clone())
+    let msg = MessageBuilder::new()
+        .mention(message.author.clone())
         .push("\n")
         .push("Commands:\n")
-        .push("`-gen [length] [word]` - ")
+        .push("`%gen [length] [word]` - ")
         .push("Generates a message, with an optional length and/or by word\n")
-        .push("`-genuser \\@mention [length]` - ")
+        .push("`%genuser \@mention [length]` - ")
         .push("Generates based on a particular user\n")
         .push("For more information on Markov chains: ")
-        .push("<https://en.wikipedia.org/wiki/Markov_chain>")
+        .push("<https://en.wikipedia.org/wiki/Markov_chain>\n")
+        .push("For more information about the bot: https://github.com/strinking/markov-bot")
         .build();
-    let _ = msg.channel_id.say(&message);
+    let _ = message.channel_id.say(&msg);
 });
